fd94db6ebe064bb6f24cb0cea908b75ae8451fec
Priyank Naik <104668352+NAIKPRIYANK@users.noreply.github.com>
2023-08-14 00:10:46 +0530

    Add files via upload

---
+++ NumberSystem/NoPrimeOrNot.java
@@ -0,0 +1,29 @@
+package NumberSystem;
+
+public class NoPrimeOrNot {
+
+	public static void main(String[] args) {
+		System.out.println(isPrime(19));
+		System.out.println(isPrime(49));
+		
+		
+
+	}
+	public static boolean isPrime(int n) {
+		if(n==0 || n==1) {
+			return false;
+			
+		}
+		if(n==2) {
+			return true;
+		}
+		for (int i =2;i<=n/2;i++) {
+			if(n%i==0) {
+				return false;
+			}
+		}
+		return true;
+		
+	}
+
+}
